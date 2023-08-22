19cff16a7eddbed4d75d99f66a218796be301002
Abdurahmon <abdurahmonavezdjanov031@gmail.com>
2023-08-22 21:33:12 +0500

    improve last navbar bags

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         className="w-[120px] cursor-pointer transition-all duration-200 hover:opacity-50 h-[100px] sm:w-[150px] sm:h-[100px] md:w-[200px] md:h-[100px]"
       />
       <nav>
-        <ul className=" sm:flex hidden items-center   text-[12px] sm:text-[15px]  gap-2 sm:gap-4">
+        <ul className=" sm:flex hidden items-center    text-[12px] sm:text-[15px]  gap-2 sm:gap-4">
           {navLinks.map((item) => {
             return (
               <li
@@ -38,37 +38,47 @@ const Header = () => {
           })}
         </ul>
         <img
-          src={toggle ? close : menu}
+          src={`${!toggle && menu}`}
           onClick={toggleHandler}
           alt="site-nav-menu-img"
-          className={`sm:hidden flex w-[30px] h-[30px] ${
-            toggle && "w-[15px] h-[15px]"
-          }`}
+          className={`${
+            toggle && `hidden`
+          }  sm:hidden  flex w-[30px] h-[30px] `}
         />
       </nav>
 
-      <ul
+      <div
         className={` ${
           toggle ? "flex" : "hidden"
-        } absolute w-full items-center  justify-evenly top-20 p-5 left-0 right-0  ${
-          style.sidebar
-        } ${style.bgBlackPrimary} `}
+        } absolute w-full top-20 p-5 left-0 right-0  ${style.sidebar} ${
+          style.bgBlackPrimary
+        }`}
       >
-        {navLinks.map((item) => {
-          return (
-            <li
-              className={`cursor-pointer    text-[13px] ss:text-[15px] ${
-                active == item.id &&
-                ` text-sky-500 transition-all ease-in  delay-300  duration-300 `
-              }`}
-              key={item.id}
-              onClick={() => setActiveHandler(item.id)}
-            >
-              {item.title}
-            </li>
-          );
-        })}
-      </ul>
+        <img
+          src={`${toggle && close}`}
+          onClick={toggleHandler}
+          alt="site-nav-menu-img"
+          className={`${
+            !toggle && `hidden`
+          } sm:hidden absolute top-[-5px] right-0 flex w-[15px] h-[15px]`}
+        />
+        <ul className={`  w-full flex items-center  justify-evenly  `}>
+          {navLinks.map((item) => {
+            return (
+              <li
+                className={`cursor-pointer    text-[13px] ss:text-[15px] ${
+                  active == item.id &&
+                  ` text-sky-500 transition-all ease-in  delay-300  duration-300 `
+                }`}
+                key={item.id}
+                onClick={() => setActiveHandler(item.id)}
+              >
+                {item.title}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </header>
   );
 };
